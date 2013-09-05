21df78be683ec0568780575c6197ffc0d4733a17
Edwin Gonzales Melquiades <edwinpgm@outlook.com>
2013-09-05 00:58:39 -0500

    asdasd

--- script.js
+++ script.js
@@ -1,24 +1,59 @@
 $(document).ready(function(){
 
-	$('.cuadrado').on('click', pintar);
-	$('.limpiar').on('click', limpiar);
+	$('.cuadrado').on('click', tres.pulsar);
+
+	$('.limpiar').on('click', tres.constructor);
+
+	$('.limpiar').on('click', tres.empezar);
 
 });
 
-function pintar() {
-	$this = $(this);
+// function pintar() {
+// 	$this = $(this);
 
-	if ( $this.text() == '' ) {
-		$this.text('o');
-		$this.addClass('o');
+// 	if ( $this.text() == '' ) {
+// 		$this.text('o');
+// 		$this.addClass('o');
+// 	}
+
+// }
+
+var tres = {
+	tamano: 3,
+	ganador: -1,
+	contar: 0,
+
+	matriz: [],
+
+	constructor: function limpiar() {
+		$('.cuadrado').each(function(i){
+			$(this).removeClass('o');
+			$(this).removeClass('x');
+			$(this).text("");
+		});
+
+		tres.empezar;
+	},
+
+	empezar: function empezarPartida() {
+		
+		for(var i = 0; i < tres.tamano; i++) {
+			tres.matriz[i] = []
+			for(var j = 0; j < tres.tamano; j++) {
+				tres.matriz[i][j] = -1;
+			}
+		}
+
+		tres.ganador = -1;
+
+		// console.log(tres.matriz);
+	},
+
+	pulsar: function pulsaBoton() {
+		$fila = $(this).data('fila');
+		$columna = $(this).data('columna');
+		console.log($(this).data());
 	}
+};
 
-}
 
-function limpiar() {
-	$('.cuadrado').each(function(i){
-		$(this).removeClass('o');
-		$(this).removeClass('x');
-		$(this).text("");
-	});
-}
